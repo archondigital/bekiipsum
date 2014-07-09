d7967a9900f66b85527c36d165b9a5ec72afbb7a
archondigital <overlord@archon-digital.com>
2014-07-09 14:25:45 +0800

    deleted unneeded files/assets

--- assets/images/legacy/v5/shadowbox-js/src/adapters/mootools.js
+++
@@ -1,1 +0,0 @@
-function getTarget(a){return a.target}function getPageXY(a){return[a.page.x,a.page.y]}function preventDefault(a){a.preventDefault()}function keyCode(a){return a.code}function addEvent(c,b,a){$(c).addEvent(b,a)}function removeEvent(c,b,a){$(c).removeEvent(b,a)};
--- assets/images/legacy/v5/shadowbox-js/src/intro.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Shadowbox.js, version 3.0.3
- * http://shadowbox-js.com/
- *
- * Copyright 2007-2010, Michael J. I. Jackson
- * 2010-03-10 00:00:00 +0000
- */
-(function(window, undefined) {
